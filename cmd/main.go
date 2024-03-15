de58630c8f9247133b3b74b1a8810c4d7f354930
Yuriy Fedyunkin <samnx39@gmail.com>
2024-03-15 14:39:34 +0200

    initial config added

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	"github.com/yuriyFedyunkin/todo-app"
 	"github.com/yuriyFedyunkin/todo-app/pkg/handler"
 	"github.com/yuriyFedyunkin/todo-app/pkg/repository"
@@ -10,12 +11,22 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializaing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
