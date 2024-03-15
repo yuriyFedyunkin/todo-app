01f19456ee9e26cd9b90b94dc5f0b814aab25b84
Yuriy Fedyunkin <samnx39@gmail.com>
2024-03-15 14:39:34 +0200

    swagger

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,6 +3,10 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/yuriyFedyunkin/todo-app/pkg/service"
+
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/files"
+	_ "github.com/yuriyFedyunkin/todo-app/docs"
 )
 
 type Handler struct {
@@ -15,6 +19,8 @@ func NewHandler(services *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	auth := router.Group("/auth")
 	{
